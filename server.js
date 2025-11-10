91c499ee01f3f406f539dc21183b235797f5f861
robiverdev <robi.verdev1@gmail.com>
2025-11-10 18:56:11 +0100

    add / route for homepage

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express")
 const app = express()
 const PORT = 4000
 
+app.get("/", (req, res) => {
+  res.send("URL Shortener Homepage")
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
