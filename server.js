1b37bee8d96d503a56bcc06e565a68a7354e71f1
robiverdev <robi.verdev1@gmail.com>
2025-11-10 19:03:24 +0100

    add /shorten route with JSON parsing

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.send("URL Shortener Homepage")
 })
 
+// Middleware - parse JSON
+app.use(express.json())
+
+app.post("/shorten", (req,res) => {
+  const {originalUrl} = req.body
+  if(!originalUrl) {
+    return res.status(400).send("URL is required!")
+  }
+  res.send("URL status OK")
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
